ac5a8f88697efb484c2ee5d3def81fb8565b5a83
mister T <aj.tinaes@icloud.com>
2025-09-25 16:12:46 +0800

    update gh pages domain and base

--- src/routes.tsx
+++ src/routes.tsx
@@ -17,33 +17,30 @@ const JamDiscovery = React.lazy(() =>
   import('@/features/jams').then((mod) => ({ default: mod.JamDiscovery }))
 );
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <AppLayout />,
-      children: [
-        { index: true, element: <DiscoveryFeed /> },
-        { path: 'login', element: 'login' },
-        { path: 'register', element: 'register' },
-        {
-          path: 'profiles',
-          children: [
-            { index: true, element: <AllProfiles /> },
-            { path: ':id', element: <Profile /> },
-          ],
-        },
-        {
-          path: 'jams',
-          children: [
-            { index: true, element: <JamDiscovery /> },
-            { path: ':id', element: <JamDetails /> },
-          ],
-        },
-      ],
-    },
-  ],
-  { basename: '/stagemates-client' }
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      { index: true, element: <DiscoveryFeed /> },
+      { path: 'login', element: 'login' },
+      { path: 'register', element: 'register' },
+      {
+        path: 'profiles',
+        children: [
+          { index: true, element: <AllProfiles /> },
+          { path: ':id', element: <Profile /> },
+        ],
+      },
+      {
+        path: 'jams',
+        children: [
+          { index: true, element: <JamDiscovery /> },
+          { path: ':id', element: <JamDetails /> },
+        ],
+      },
+    ],
+  },
+]);
 
 export default router;
